7eca540809a3f59afc2a3551fb7190ae4d4cfccf
YeonJu <yjnoh1016@naver.com>
2021-09-01 17:11:00 +0900

    [UPDATE] addTarget backButton

--- Arr/Controllers/ForgotPasswordViewController.swift
+++ Arr/Controllers/ForgotPasswordViewController.swift
@@ -78,6 +78,7 @@ class ForgotPasswordViewController: UIViewController{
         $0.setTitle("로그인 하러 가기", for: .normal)
         $0.setTitleColor(.rgb(red: 153, green: 153, blue: 153), for: .normal)
         $0.dynamicFont(fontSize: 14, currentFontName: "SeoulNamsanM")
+        $0.addTarget(self, action: #selector(goBack), for: .touchUpInside)
     }
     
     //MARK: - Lifecycle
