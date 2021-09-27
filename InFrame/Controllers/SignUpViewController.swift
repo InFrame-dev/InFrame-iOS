2613431a5c0193c3a7cc1dcccfeb412940987110
Y00ujin <yujingim43@gmail.com>
2021-09-27 18:15:11 +0900

    주석 추가

---
+++ InFrame/Controllers/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  InFrame
+//
+//  Created by 김유진 on 2021/09/27.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    //MARK: - Helpers
+    func configureUI(){
+        addView()
+        
+        addLayout()
+    }
+    
+    //MARK: - addView
+    func addView(){
+        
+    }
+    
+    //MARK: - addLayout
+    func addLayout(){
+        
+    }
+    
+    //MARK: - Selectors
+}
