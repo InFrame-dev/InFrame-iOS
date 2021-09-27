14320554f697c6d5f16b2ecbe476159154d18b00
Y00ujin <yujingim43@gmail.com>
2021-09-27 20:13:36 +0900

    agreeLabel 추가

--- InFrame/Controllers/TermsOfServiceViewController.swift
+++ InFrame/Controllers/TermsOfServiceViewController.swift
@@ -17,8 +17,15 @@ class TermsOfServiceViewController: UIViewController {
     
     private lazy var agreeLabel = UILabel().then{
         $0.text = "이용약관에 동의해주세요!"
-        $0.dynamicFont(fontSize: 30, currentFontName: "AppleSDGothicNeo-Thin")
+        $0.dynamicFont(fontSize: 16, currentFontName: "AppleSDGothicNeo-Thin")
         $0.textColor = .black
+    }
+    
+    private let allAgreeBackgroundView = UIView().then{
+        $0.backgroundColor = UIColor.rgb(red: 254, green: 254, blue: 254)
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.rgb(red: 167, green: 167, blue: 167).cgColor
+        $0.layer.cornerRadius = 5
     }
     
     private let termsOfServiceTitleLabelColorArray = [UIColor.rgb(red: 126, green: 152, blue: 212), UIColor.rgb(red: 251, green: 186, blue: 200)]
@@ -41,6 +48,7 @@ class TermsOfServiceViewController: UIViewController {
     func addView(){
         self.view.backgroundColor = .white
         self.view.addSubview(termsOfServiceTitleLabel)
+        self.view.addSubview(agreeLabel)
     }
     
     //MARK: - addLayout
@@ -48,6 +56,11 @@ class TermsOfServiceViewController: UIViewController {
         termsOfServiceTitleLabel.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(self.view.frame.width/5.75)
             make.top.equalToSuperview().offset(self.view.frame.height/5.68)
+        }
+        
+        agreeLabel.snp.makeConstraints { make in
+            make.left.equalTo(termsOfServiceTitleLabel)
+            make.top.equalTo(termsOfServiceTitleLabel.snp.bottom)
         }
     }
 }
