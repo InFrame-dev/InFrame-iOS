c6020b528175eb39675877b71a37830202fc69d0
Y00ujin <yujingim43@gmail.com>
2021-09-28 13:44:22 +0900

    FindPasswardEmailViewController 추가

---
+++ InFrame/Controllers/FindPasswardEmailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FindPasswardEmailViewController.swift
+//  InFrame
+//
+//  Created by 김유진 on 2021/09/28.
+//
+
+import UIKit
+
+class FindPasswardEmailViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configure()
+    }
+    
+    // MARK: - Helpers
+    func configure(){
+        addView()
+        
+        addLayout()
+    }
+    
+    // MARK: - addView
+    func addView(){
+        
+    }
+    
+    // MARK: - addLayout
+    func addLayout(){
+        
+    }
+    
+
+    // MARK: - Selectors
+
+}
