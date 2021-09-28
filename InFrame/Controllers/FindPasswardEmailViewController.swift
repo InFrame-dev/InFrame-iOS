2a33666087ddf265491ad09fbb5595f97a070bc9
Y00ujin <yujingim43@gmail.com>
2021-09-28 13:50:58 +0900

    요소들을 다 선언했어요~

--- InFrame/Controllers/FindPasswardEmailViewController.swift
+++ InFrame/Controllers/FindPasswardEmailViewController.swift
@@ -9,6 +9,66 @@ import UIKit
 
 class FindPasswardEmailViewController: UIViewController {
     // MARK: - Properties
+    private let findPasswardTitleLabel = UILabel().then{
+        $0.text = "Find Passward"
+        $0.dynamicFont(fontSize: 30, currentFontName: "CarterOne")
+        $0.updateGradientTextColor_horizontal(gradientColors: [UIColor.rgb(red: 126, green: 152, blue: 212), UIColor.rgb(red: 251, green: 186, blue: 200)])
+    }
+    
+    private let writeEmailLabel = UILabel().then{
+        $0.text = "이메일을 입력해주세요!"
+        $0.dynamicFont(fontSize: 16, currentFontName: "AppleSDGothicNeo-Thin")
+    }
+    
+    private lazy var emailTitleLabel = UILabel().then{
+        $0.text = "Email"
+        $0.dynamicFont(fontSize: 13, currentFontName: "CarterOne")
+        $0.updateGradientTextColor_horizontal(gradientColors: [UIColor.rgb(red: 126, green: 152, blue: 212), UIColor.rgb(red: 251, green: 186, blue: 200)])
+    }
+    
+    private let emailTextField = UITextField().then{
+        $0.placeholder = "이메일을 입력해주세요."
+        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-Medium")
+        $0.textColor = UIColor.rgb(red: 123, green: 123, blue: 123)
+    }
+    
+    private lazy var emailLineView = UIView().then{
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width/1.4, height: 1))
+        let gradient = CAGradientLayer()
+
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor(red: 104/255, green: 134/255, blue: 197/255, alpha: 1).cgColor,UIColor(red: 255/255, green: 173/255, blue: 172/255, alpha: 1).cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+
+        $0.layer.insertSublayer(gradient, at: 0)
+        $0.clipsToBounds = true
+    }
+    
+    private let emailCheckButton = UIButton().then{
+        $0.backgroundColor = UIColor.rgb(red: 222, green: 222, blue: 222)
+        $0.setTitle("이메일 인증", for: .normal)
+        $0.dynamicFont(fontSize: 6.5, currentFontName: "AppleSDGothicNeo-Bold")
+    }
+    
+    lazy var nextButton = UIButton().then{
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width/1.4, height: self.view.frame.height/19.80))
+        let gradient = CAGradientLayer()
+
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor(red: 126/255, green: 152/255, blue: 212/255, alpha: 1).cgColor,UIColor(red: 250/255, green: 186/255, blue: 200/255, alpha: 1).cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+
+        $0.layer.insertSublayer(gradient, at: 0)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 10
+        $0.setTitle("다 음", for: .normal)
+        $0.dynamicFont(fontSize: 13, currentFontName: "AppleSDGothicNeo-Bold")
+        $0.addTarget(self, action: #selector(nextButtonClicked(sender:)), for: .touchUpInside)
+    }
     
     // MARK: - LifeCycles
     override func viewDidLoad() {
@@ -26,7 +86,7 @@ class FindPasswardEmailViewController: UIViewController {
     
     // MARK: - addView
     func addView(){
-        
+        self.view.backgroundColor = .white
     }
     
     // MARK: - addLayout
@@ -36,5 +96,7 @@ class FindPasswardEmailViewController: UIViewController {
     
 
     // MARK: - Selectors
-
+    @objc func nextButtonClicked(sender:UIButton){
+        
+    }
 }
