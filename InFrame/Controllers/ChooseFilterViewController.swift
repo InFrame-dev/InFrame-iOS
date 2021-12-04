918b3009eb253be8d7333c4573f8dadad8a2b0e4
노연주 <yjnoh1016@naver.com>
2021-12-05 00:58:05 +0900

    [CREATE] ChooseFilterVC

---
+++ InFrame/Controllers/ChooseFilterViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ChooseFilterViewController.swift
+//  InFrame
+//
+//  Created by 노연주 on 2021/12/05.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class ChooseFilterViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        location()
+    }
+    
+    // MARK: - Add View
+    
+    private func addView(){
+        
+    }
+    
+    // MARK: - Location
+    
+    private func location(){
+        
+    }
+    
+}
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct ChooseFilterViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        ChooseFilterViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ChooseFilterViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
