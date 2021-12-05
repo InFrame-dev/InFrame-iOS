eda4ec021c6ee09833b9187fca0b8ec2a9f77635
노연주 <yjnoh1016@naver.com>
2021-12-05 13:35:08 +0900

    [UPDATE] Delete PreviewCode

--- InFrame/Controllers/ChooseFilterViewController.swift
+++ InFrame/Controllers/ChooseFilterViewController.swift
@@ -110,29 +110,3 @@ class ChooseFilterViewController: UIViewController {
     }
     
 }
-
-//MARK: - Preview
-#if DEBUG
-import SwiftUI
-struct ChooseFilterViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        ChooseFilterViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ChooseFilterViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
