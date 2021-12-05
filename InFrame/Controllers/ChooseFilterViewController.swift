9d250258f0a0a50ca2a7fb4b1173975d8e78e4d2
노연주 <yjnoh1016@naver.com>
2021-12-05 13:22:19 +0900

    [UPDATE] Add filterButtonStackView

--- InFrame/Controllers/ChooseFilterViewController.swift
+++ InFrame/Controllers/ChooseFilterViewController.swift
@@ -14,9 +14,28 @@ class ChooseFilterViewController: UIViewController {
     
     private let imageFilterView = ImageFilterView()
     
+    private let blackButton = FilterButton().then {
+        $0.dataSetting(image: "InFrame_FilterBlack", koreanText: "흑백", englishText: "BLACK")
+    }
+    
+    private let basicButton = FilterButton().then {
+        $0.dataSetting(image: "InFrame_FilterBasic", koreanText: "기본", englishText: "BASIC")
+    }
+    
+    private let lightButton = FilterButton().then {
+        $0.dataSetting(image: "InFrame_FilterLight", koreanText: "밝게", englishText: "LIGHT")
+    }
+    
+    private lazy var filterButtonStackView = UIStackView(arrangedSubviews: [blackButton, basicButton, lightButton]).then {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = self.view.frame.width/26.79
+    }
+    
     private let chooseFrameButton = ChoiceGradientButton().then {
         $0.dataSetting(buttonText: "프레임 선택하러 가기")
     }
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +54,7 @@ class ChooseFilterViewController: UIViewController {
     // MARK: - Add View
     
     private func addView(){
-        [imageFilterView, chooseFrameButton].forEach { view.addSubview($0) }
+        [imageFilterView, filterButtonStackView, chooseFrameButton].forEach { view.addSubview($0) }
     }
     
     // MARK: - Location
@@ -48,12 +67,20 @@ class ChooseFilterViewController: UIViewController {
             make.height.equalToSuperview().dividedBy(3.64)
         }
         
+        filterButtonStackView.snp.makeConstraints { make in
+            make.top.equalTo(imageFilterView.snp.bottom).offset(self.view.frame.height/10.68)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().dividedBy(1.21)
+            make.height.equalToSuperview().dividedBy(6.44)
+        }
+        
         chooseFrameButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview().inset(self.view.frame.height/12.5)
             make.height.equalToSuperview().dividedBy(19.80)
             make.left.equalToSuperview().offset(self.view.frame.width/8.52)
         }
+
     }
     
 }
