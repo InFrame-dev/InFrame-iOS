cfcff00e8e784594fbe8c533ac68bad0153ff8f7
Y00ujin <yujingim43@gmail.com>
2021-12-15 16:36:44 +0900

    [CREATE] NewPasswordEmailCheckViewController created

--- InFrame/Controllers/NewPasswordEmailCheckViewController.swift
+++ InFrame/Controllers/NewPasswordEmailCheckViewController.swift
@@ -13,31 +13,31 @@ class NewPasswordEmailCheckViewController: UIViewController{
         $0.setImage(UIImage(named: "InFrame_BackButtonImage"), for: .normal)
         $0.addTarget(self, action: #selector(backButtonClicked(sender:)), for: .touchUpInside)
     }
-    
     private let findPasswardTitleLabel = UILabel().then{
         $0.text = "Find Password"
         $0.dynamicFont(fontSize: 30, currentFontName: "CarterOne")
         $0.updateGradientTextColor_horizontal(gradientColors: [UIColor.rgb(red: 126, green: 152, blue: 212), UIColor.rgb(red: 251, green: 186, blue: 200)])
     }
-    
-    private let checkEmailLabel = UILabel().then{
-        $0.text = "이메일의 확인버튼을 클릭해주세요!"
-        $0.dynamicFont(fontSize: 16, currentFontName: "AppleSDGothicNeo-Thin")
-    }
-    
     private let nextButton = GradientButton().then{
         $0.dataSetting(buttonText: "확인버튼 클릭했어요!")
         $0.addTarget(self, action: #selector(nextButtonClicked(sender:)), for: .touchUpInside)
     }
-    
-    private let emailImageView = UIImageView().then{
-        $0.image = UIImage(named: "InFrame_EmailCheckImage")
+    private let introLabel = UILabel().then{
+        $0.text = "이메일로 전송된 코드 6자리를 입력해주세요!"
+        $0.dynamicFont(fontSize: 13, currentFontName: "AppleSDGothicNeo-UltraLight")
+        $0.textColor = UIColor.rgb(red: 106, green: 106, blue: 106)
     }
-    
-    private let imageBackgroundView = UIView().then{
-        $0.backgroundColor = .white
-        $0.layer.applySketchShadow(x: 0, y: 4, blur: 20)
-        $0.layer.cornerRadius = 13
+    private let codeBackgroundView = UIView().then{
+        $0.backgroundColor = UIColor.rgb(red: 250, green: 250, blue: 250)
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.rgb(red: 225, green: 225, blue: 225).cgColor
+        $0.layer.cornerRadius = 5
+    }
+    private let codeTextField = UITextField().then{
+        $0.placeholder = "코드 6자리를 입력해주세요."
+        $0.textColor = UIColor.rgb(red: 152, green: 152, blue: 152)
+        $0.dynamicFont(fontSize: 11, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textAlignment = .center
     }
     
     // MARK: - LifeCycles
@@ -45,6 +45,10 @@ class NewPasswordEmailCheckViewController: UIViewController{
         super.viewDidLoad()
         
         configureUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        codeTextField.becomeFirstResponder()
     }
     
     // MARK: - Helpers
@@ -57,8 +61,8 @@ class NewPasswordEmailCheckViewController: UIViewController{
     
     // MARK: - addView
     private func addView(){
-        [backButton, findPasswardTitleLabel, checkEmailLabel, nextButton, imageBackgroundView].forEach { view.addSubview($0) }
-        [emailImageView].forEach { imageBackgroundView.addSubview($0) }
+        [backButton, findPasswardTitleLabel, nextButton, introLabel, codeBackgroundView].forEach { view.addSubview($0) }
+        [codeTextField].forEach { codeBackgroundView.addSubview($0) }
     }
     
     // MARK: - location
@@ -71,26 +75,26 @@ class NewPasswordEmailCheckViewController: UIViewController{
             make.left.equalTo(backButton)
             make.top.equalTo(backButton.snp.bottom).offset(self.view.frame.height/10.54)
         }
-        checkEmailLabel.snp.makeConstraints { make in
-            make.left.equalTo(findPasswardTitleLabel)
-            make.top.equalTo(findPasswardTitleLabel.snp.bottom)
+        introLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(findPasswardTitleLabel.snp.bottom).offset(self.view.frame.height/14)
         }
-        imageBackgroundView.snp.makeConstraints { make in
-            make.top.equalTo(checkEmailLabel.snp.bottom).offset(self.view.frame.height/20)
-            make.left.equalTo(checkEmailLabel)
-            make.centerX.equalToSuperview()
-            make.height.equalToSuperview().dividedBy(4.34)
+        codeBackgroundView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(introLabel.snp.bottom).offset(self.view.frame.height/45.11)
+            $0.width.equalToSuperview().dividedBy(2.35)
+            $0.height.equalToSuperview().dividedBy(17.6)
         }
-        emailImageView.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-self.view.frame.height/320)
-            make.width.height.equalToSuperview().dividedBy(1.1)
-            make.centerX.equalToSuperview()
+        codeTextField.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(1.3)
+            $0.height.equalToSuperview().dividedBy(2)
         }
         nextButton.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(self.view.frame.width/6.46)
             make.centerX.equalToSuperview()
             make.height.equalToSuperview().dividedBy(19.8)
-            make.top.equalTo(imageBackgroundView.snp.bottom).offset(self.view.frame.height/19.8)
+            make.top.equalTo(codeTextField.snp.bottom).offset(self.view.frame.height/19.8)
         }
     }
     
