50c3543c5773c7aee63fbbc972292d074b1f8644
Y00ujin <yujingim43@gmail.com>
2022-05-26 22:21:26 +0900

    [DELETE] deleted trash files

--- InFrame/Controllers/Youjin.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Youjin.swift
-//  InFrame
-//
-//  Created by 김유진 on 2022/05/26.
-//
-
-import Foundation
--- InFrame/Controllers/Youjing.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Youjing.swift
-//  InFrame
-//
-//  Created by 김유진 on 2022/05/26.
-//
-
-import Foundation
